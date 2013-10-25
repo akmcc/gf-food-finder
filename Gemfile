ff3706a689ffd111072174930a29138273a95f13
Ashton McCrate <a.mccrate@gmail.com>
2013-10-25 13:12:46 -0400

    more file organization

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'sinatra', require: 'sinatra/base'
+gem 'sinatra-contrib', require: 'sinatra/reloader'
+
 gem 'thin'
 gem 'oauth'
 
--- app.rb
+++ app.rb
@@ -1,31 +1,27 @@
 require 'json'
 require 'cgi'
-require 'sinatra'
-require 'sinatra/reloader'
-require_relative '../../secrets'
+require './secrets'
 
 
 
-
-get "/" do
-  @craving = params['craving']
-  @location = params['location']
-  if !@craving.nil? && !@location.nil?
-    gf_places = get_businesses["businesses"]
+class FoodFinder < Sinatra::Base
+  get "/" do
+    @craving = params['craving']
+    @location = params['location']
+    if !@craving.nil? && !@location.nil?
+      gf_places = get_businesses["businesses"]
+    end
+    erb :index, :locals => {:gf_places => gf_places}
   end
-  erb :index, :locals => {:gf_places => gf_places}
-end
 
-def get_businesses
-  
+  def get_businesses
     path = "/v2/search?term=#{CGI.escape(@craving)}&location=#{CGI.escape(@location)}&sort=2&category_filter=gluten_free&limit=10"
-    
+
     api_host = 'api.yelp.com'
 
     consumer = OAuth::Consumer.new($CONSUMER_KEY, $CONSUMER_SECRET, {:site => "http://#{api_host}"})
     @access_token = OAuth::AccessToken.new(consumer, $TOKEN, $TOKEN_SECRET)
 
-   JSON.parse(@access_token.get(path).body)
-   
-  
+    JSON.parse(@access_token.get(path).body)  
+  end
 end
