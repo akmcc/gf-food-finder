cc6debf4f1e606af3ab9cfb83f7e2478637afb54
Ashton McCrate <a.mccrate@gmail.com>
2013-11-05 19:05:04 -0500

    extract oauth out of restaurants and in to own module

--- restaurants.rb
+++ restaurants.rb
@@ -1,20 +1,33 @@
-require 'json'
-require 'cgi'
-require './secrets'
+module YelpOauth
+
+  def query_for_businesses
+    create_consumer
+    create_access_token
+    create_path
+    (JSON.parse(@access_token.get(@path).body))['businesses']
+  end
+
+  def create_consumer
+    api_host = 'api.yelp.com'
+    @consumer = OAuth::Consumer.new($CONSUMER_KEY, $CONSUMER_SECRET, {:site => "http://#{api_host}"})    
+  end
+
+  def create_access_token
+    @access_token = OAuth::AccessToken.new(@consumer, $TOKEN, $TOKEN_SECRET)
+  end
+
+  def create_path
+    @path = "/v2/search?term=#{CGI.escape(@craving)}&location=#{CGI.escape(@location)}&sort=2&category_filter=gluten_free&limit=10"
+  end
+
+end
 
 class Restaurants
+  include YelpOauth
 
   def initialize(craving, location)
     @craving = craving
     @location = location
-  end
-
-  def query_yelp
-    path = "/v2/search?term=#{CGI.escape(@craving)}&location=#{CGI.escape(@location)}&sort=2&category_filter=gluten_free&limit=10"
-    api_host = 'api.yelp.com'
-    consumer = OAuth::Consumer.new($CONSUMER_KEY, $CONSUMER_SECRET, {:site => "http://#{api_host}"})
-    @access_token = OAuth::AccessToken.new(consumer, $TOKEN, $TOKEN_SECRET)
-    JSON.parse(@access_token.get(path).body)  
   end
 
 end
