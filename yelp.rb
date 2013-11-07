d34a7b4ac027ff06160885f9b6847da1fa719ade
Ashton McCrate <a.mccrate@gmail.com>
2013-11-07 14:43:52 -0500

    move yelp stuff to own file

---
+++ yelp.rb
@@ -0,0 +1,23 @@
+module YelpOauth
+
+  def query_for_businesses
+    create_consumer
+    create_access_token
+    create_path
+    (JSON.parse(@access_token.get(@path).body))['businesses']
+  end
+
+  def create_consumer
+    api_host = 'api.yelp.com'
+    @consumer = OAuth::Consumer.new($CONSUMER_KEY, $CONSUMER_SECRET, {:site => "http://#{api_host}"})    
+  end
+
+  def create_access_token
+    @access_token = OAuth::AccessToken.new(@consumer, $TOKEN, $TOKEN_SECRET)
+  end
+
+  def create_path
+    @path = "/v2/search?term=#{CGI.escape(@craving)}&location=#{CGI.escape(@location)}&sort=2&category_filter=gluten_free&limit=10"
+  end
+
+end
